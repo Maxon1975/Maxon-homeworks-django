4c625a4945b43874f4e0f97c73836c6dbf386663
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-02-15 16:57:34 +0300

    save 2

--- 3.1-drf-intro/smart_home/measurement/apps.py
+++ 3.1-drf-intro/smart_home/measurement/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class MeasurementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'measurement'
+class MeasurementsConfig(AppConfig):
+    name = 'measurements'
